a08a3c73b186fccb52c146d0ae403b3f6dd502ab
Destin <dleeinc@gmail.com>
2021-11-24 18:29:52 -0600

    v0.1.0

--- README.md
+++ README.md
@@ -2,4 +2,28 @@
 
 `npx make-types-rest`
 
-## More soon... 🚀
+```sh
+ Usage
+    $ mtr --url <urlString>
+    $ mtr --url <urlString> --copy
+    $ mtr --url <urlString> --root post
+
+  Options
+    --url, -u  Url to be converted to Types
+     --root, -r Name of Interface default "RootObject"
+    --copy, -c Copy output to clipboard
+
+  Examples
+    $ mtr -u https://jsonplaceholder.typicode.com/posts  --root post
+
+     interface Post {
+       userId: number;
+       id: number;
+       title: string;
+       body: string;
+     }
+```
+
+## Creds
+
+json-to-ts - https://www.npmjs.com/package/json-to-ts
--- index.js
+++ index.js
@@ -2,23 +2,76 @@
 
 import jsonToTs from 'json-to-ts'
 import fetch from 'node-fetch'
+import meow from 'meow'
+import clipboard from 'clipboardy'
+import chalk from 'chalk'
+import cfonts from 'cfonts'
+;(async () => {
+  const app = meow(
+    `
+	Usage
+	  $ mtr --url <urlString>
+	  $ mtr --url <urlString> --copy
+	  $ mtr --url <urlString> --root post
 
-async function getJson(rootName) {
+	Options
+	  --url, -u  Url to be converted to Types
+    --root, -r Name of Interface default "RootObject"
+	  --copy, -c Copy output to clipboard
+
+	Examples
+	  $ mtr -u https://jsonplaceholder.typicode.com/posts  --root post
+
+    interface Test {
+      userId: number;
+      id: number;
+      title: string;
+      body: string;
+    }
+
+`,
+    {
+      importMeta: import.meta,
+      flags: {
+        url: {
+          type: 'string',
+          alias: 'u',
+          isRequired: true,
+        },
+        copy: {
+          type: 'boolean',
+          alias: 'c',
+        },
+        root: {
+          type: 'string',
+          alias: 'r',
+        },
+      },
+    }
+  )
+
+  const { url, copy, root } = app.flags
+
+  if (!url) {
+    console.log(app.help)
+    return
+  }
+
   const res = await fetch('https://jsonplaceholder.typicode.com/posts')
-  const data = await res.json()
+  const json = await res.json()
+  const data = await json[0]
 
-  return data[0]
-}
+  const types = !root
+    ? jsonToTs(data)[0]
+    : jsonToTs(data, {
+        rootName: root,
+      })[0]
 
-/**
- *
- * @param {string} rootName - Root type name
- * @example getTypes('Posts')
- */
-async function getTypes(rootName) {
-  const convertedTypes = jsonToTs()
-}
+  console.log(chalk.bold.yellowBright('\nHere are the types you requested:\n'))
+  console.log(chalk.cyanBright(types))
 
-;(() => {
-  console.log('More soom 🚀')
+  if (copy) {
+    clipboard.writeSync(types)
+    console.log(chalk.greenBright.bold('\nCopied to you clipbaord 🚀\n'))
+  }
 })()
